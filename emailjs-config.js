12bab0df22c9e9d137c20ce849bae13950a9f80e
Mubin Shafeek Shaikh <mubinshaikh2013@gmail.com>
2026-02-02 14:30:55 +0530

    Email Chnages

--- emailjs-config.js
+++ emailjs-config.js
@@ -1,5 +1,5 @@
 // EmailJS Configuration
-// Replace these with your actual EmailJS credentials
+// Replace these with your actual EmailJS credentials //
 
 const EMAILJS_PUBLIC_KEY = 'zYmO_PC5HF56EZtIk';
 const EMAILJS_SERVICE_ID = 'service_v44j566';
